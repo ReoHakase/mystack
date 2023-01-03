ee4c3c01900d09c5facb2c49bf64eb90b957dad8
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-01-03 21:05:23 +0900

    initial: 🎉

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,12 @@
+// Install `next` to the root of the project in order to avoid the following error:
+// > error - ESLint: Failed to load config "next/babel" to extend from.
+// Refer: https://github.com/vercel/next.js/issues/40687
+// Sadly, this is an only workaround for now.
+
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier', 'next/babel'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
   },
 };
