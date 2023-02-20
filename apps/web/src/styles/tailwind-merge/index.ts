047e6cd8cf95be8b57b90a2f65c3fd1522272e43
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-02-20 22:41:45 +0900

    feat: ✨ (twMerge) `tailwind-merge`を導入した。

---
+++ apps/web/src/styles/tailwind-merge/index.ts
@@ -0,0 +1,17 @@
+import { extendTailwindMerge } from 'tailwind-merge';
+
+export const twMerge = extendTailwindMerge({
+  // ↓ Add values to existing theme scale or create a new one
+  // theme: {
+  //   spacing: ['sm', 'md', 'lg'],
+  // },
+  // ↓ Add values to existing class groups or define new ones
+  // classGroups: {
+  //   foo: ['foo', 'foo-2', { 'bar-baz': ['', '1', '2'] }],
+  //   bar: [{ qux: ['auto', (value: unknown) => Number(value) >= 1000] }],
+  // },
+  // ↓ Here you can define additional conflicts across class groups
+  // conflictingClassGroups: {
+  //   foo: ['bar'],
+  // },
+});
