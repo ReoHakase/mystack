f3dd85642b4ee5611d49bca1be068d15680eab3c
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-02-27 07:47:13 +0900

    fix: 🐛 (web, Tailwind) カラースキームに合わせてパレットを入れ替える`tw-colors`のために`next-themes`を設定しなおした。

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -7,11 +7,12 @@ const darkTokens = require('tokens-studio/dist/tailwind-css/dark.json');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // Used with `next-themes`
+  darkMode: ['class', '[data-theme="dark"]'], // Used with `next-themes`
   theme: {
     colors: coreTokens.colors,
   },
   plugins: [
+    // `tw-colors` only works with dark mode selectors by data attribute
     createThemes({
       light: lightTokens.colors,
       dark: darkTokens.colors,
