2367fea8810ed84001983e1e58390b359476313c
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-02-27 07:28:21 +0900

    chore: 🔧 (web, Tailwind) `tailwind.config.js`にTokens Studio由来の色を追加した。

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,20 @@
+const { createThemes } = require('tw-colors');
+
+const coreTokens = require('tokens-studio/dist/tailwind-css/core.json');
+const lightTokens = require('tokens-studio/dist/tailwind-css/light.json');
+const darkTokens = require('tokens-studio/dist/tailwind-css/dark.json');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // Used with `next-themes`
   theme: {
-    extend: {},
+    colors: coreTokens.colors,
   },
-  plugins: [],
+  plugins: [
+    createThemes({
+      light: lightTokens.colors,
+      dark: darkTokens.colors,
+    }),
+  ],
 };
