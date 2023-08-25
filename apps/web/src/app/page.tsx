03265b59ab4d3447582e9378dc1b5c02165a7b6f
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2023-08-25 20:32:34 +0900

    wip: 🚧 フォントを追加した。

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,7 @@ const Home: FC = () => {
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-keyplate-12">
       <article className="flex max-w-2xl flex-col gap-4 p-6">
         <h1 className="my-6 text-5xl font-bold text-keyplate-12">ACMEへようこそ！</h1>
-        <section>
+        <section className="font-sans">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
